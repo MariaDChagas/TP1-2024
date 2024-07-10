bd872647da1c0c09ead2fb8fdd6c25b0c87a8567
MariaDChagas <143622576+MariaDChagas@users.noreply.github.com>
2024-07-10 10:34:15 -0300

    Exercício 10/07

---
+++ servidor.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded ({ extended: true}))
+app.set('view engine', 'ejs')
+
+app.get('/conta', (request, response) => {
+    response.render ('conta')
+})
+
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log("rodano =D")
+})
+
