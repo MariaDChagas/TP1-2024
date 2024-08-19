a3257a0482cfbb3494ad3502e00900c76c3add34
MariaDChagas <143622576+MariaDChagas@users.noreply.github.com>
2024-08-19 11:11:53 -0300

    19/08 Ajustes no servidor + Adicionando uma seção de cabeçalho e arquivo css

--- servidor.js
+++ servidor.js
@@ -9,29 +9,87 @@ app.get('/conta', (request, response) => {
 })
 
 app.get('/cal', (request, response) => {
-    resultado = ''
-    response.render ('calculo')
+    response.render ('calculo', {n1:"", n2: "", resultado: ""})
 })
 
-app.post('/soma', (request, response) => {
-    soma = parseFloat(request.body.n1) + parseFloat(request.body.n2)
-    response.render('calculo', {resultado: soma})
+app.route('/erro')
+.get((request, response) => {
+    response.render ('erro')
 })
 
-app.post('/sub', (request, response) => {
-    sub = parseFloat(request.body.n1) - parseFloat(request.body.n2)
-    response.render('calculo', {resultado: sub})
+app.route('/soma')
+.get ((request, response) => {
+  response.redirect("/cal")
+}) 
+.post((request,response) => {     
+n1 = parseFloat(request.body.n1)
+n2 = parseFloat(request.body.n2)
+
+if (isNaN (n1) || isNaN (n2)){
+resultado = `Digite apenas números.`
+response.redirect("/erro")
+} else {
+resultado = n1 + n2
+response.render('calculo')
+}
 })
 
-app.post('/mult', (request, response) => {
-    mult = parseFloat(request.body.n1) * parseFloat(request.body.n2)
-    response.render('calculo', {resultado: mult}) 
+app.route('/sub')
+.get ((request, response) => {
+  response.redirect("/cal")
+}) 
+.post((request,response) => {     
+n1 = parseFloat(request.body.n1)
+n2 = parseFloat(request.body.n2)
+
+if (isNaN (n1) || isNaN (n2)){
+resultado = `Digite apenas números.`
+response.redirect("/erro")
+} else {
+resultado = n1 - n2
+response.render('calculo')
+}
 })
 
-app.post('/div', (request, response) => {
-    div = parseFloat(request.body.n1) / parseFloat(request.body.n2)
-    response.render('calculo', {resultado: div})
-})
+app.route('/mult')
+.get ((request, response) => {
+  response.redirect("/cal")
+}) 
+.post((request,response) => {     
+    n1 = parseFloat(request.body.n1)
+    n2 = parseFloat(request.body.n2)
+    
+    if (isNaN (n1) || isNaN (n2)){
+    resultado = `Digite apenas números.`
+    response.redirect("/erro")
+    } else {
+    resultado = n1 * n2
+    response.render('calculo')
+    }
+    })
+    
+
+app.route('/div')
+.get ((request, response) => {
+  response.redirect("/cal")
+}) 
+.post((request,response) => {     
+    n1 = parseFloat(request.body.n1)
+    n2 = parseFloat(request.body.n2)
+    
+    if (isNaN (n1) || isNaN (n2)){
+    resultado = `Digite apenas números.`
+    response.redirect("/erro")
+    } else if(n2 == 0) {
+    resultado = `Nenhum número pode ser dividido por 0.`
+    response.redirect("/erro")
+    } 
+    else{
+    resultado = n1 / n2
+    response.render('calculo')
+    }
+    })
+    
 
 const PORT = 8080
 app.listen(PORT, () => {
